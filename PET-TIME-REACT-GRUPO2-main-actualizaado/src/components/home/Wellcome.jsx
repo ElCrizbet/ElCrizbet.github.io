77c9148e5afc3ffc307bddf226c08b29f545dd8a
ElCrizbet <89428222+ElCrizbet@users.noreply.github.com>
2026-04-13 21:04:53 -0500

    Delete PET-TIME-REACT-GRUPO2-main-actualizaado directory

--- PET-TIME-REACT-GRUPO2-main-actualizaado/src/components/home/Wellcome.jsx
+++
@@ -1,9 +0,0 @@
-export function HomeWellcome() {
-    return (
-        <div className="flex flex-col px-7.5 py-5 gap-1.5 bg-[#32ACDC] text-white rounded-[10px] m-5">
-            <span>Hola 👋</span>
-            <span className="font-bold">Bienvenido...!</span>
-            <span>¿Qué necesita tu mascota hoy?</span>
-        </div>
-    )
-}
